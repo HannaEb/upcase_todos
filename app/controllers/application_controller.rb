d352b35fe3291f1a45015e09a370e3fbb1d36d7b
Hanna <hannaeberhoefer@yahoo.com>
2016-08-20 14:59:45 +0100

    Refactored session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
 
   def authenticate
@@ -10,6 +9,14 @@ class ApplicationController < ActionController::Base
   end
 
   def signed_in?
-    session[:current_email].present?
+    current_email.present?
+  end
+
+  def current_email
+    session[:current_email]
+  end
+
+  def sign_in_as(email)
+    session[:current_email] = email
   end
 end
